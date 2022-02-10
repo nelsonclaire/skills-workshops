d7f9f5cc359c954a712884a43ead0cc85ba4978c
Claire Nelson <claire.nelson@johnlewis.co.uk>
2022-02-10 12:47:46 +0000

    corrected three errors using debugging method

--- how_the_web_works/debugging_blog_app/app.rb
+++ how_the_web_works/debugging_blog_app/app.rb
@@ -15,21 +15,21 @@ class BlogApp < Sinatra::Base
   end
 
   get '/' do
-    posts = @post_manager.all_posts
+    @posts = @post_manager.all_posts
 
     erb :index
   end
 
   # get posts for a given tag
-  post '/tag/:tag' do
-    @posts = @post_manager.all_posts_by_tag(params[:tag])
+  get '/tag/:tags' do
+    @posts = @post_manager.all_posts_by_tag(params[:tags])
 
     erb :index
   end
 
   # create new post
   post '/posts' do
-    new_post = Post.new(params[:the_title], params[:content], params[:tags].split(','))
+    new_post = Post.new(params[:title], params[:content], params[:tags].split(','))
     @post_manager.add_post(new_post)
 
     redirect '/'
